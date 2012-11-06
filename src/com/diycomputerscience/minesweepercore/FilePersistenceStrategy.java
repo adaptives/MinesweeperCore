39a5c8417db161bb82d78d48b3302a464f616759
Parag Shah <adaptives@gmail.com>
2012-11-06 14:20:52 +0530

    added comments

--- src/com/diycomputerscience/minesweepercore/FilePersistenceStrategy.java
+++ src/com/diycomputerscience/minesweepercore/FilePersistenceStrategy.java
@@ -59,21 +59,30 @@ public class FilePersistenceStrategy implements PersistenceStrategy {
 		Square squares[][] = new Square[Board.MAX_ROWS][Board.MAX_COLS];
 		
 		try {
+			// Get a reader to the file
 			BufferedReader reader = getReader();
+			// Compile the regex pattern
 			Pattern regexPattern = Pattern.compile(SQUARE_LOAD_REGEX);
 			String line = "";
+			// For every line
 			while((line = reader.readLine()) != null) {
+				// Create a matcher object to perform matching on the line using the regex pattern
 				Matcher matcher = regexPattern.matcher(line);
 				matcher.find();
+				// Get the first matching group. ie the pattern enclosed in the first () representing the row co-ordinate
 				String sRow = matcher.group(1);
 				int row = Integer.parseInt(sRow);
+				// Get the second matching group, representing the col co-ordinate
 				String sCol = matcher.group(2);
 				int col = Integer.parseInt(sCol);
+				// Get the third matching group, representing whether the square is a mine
 				String sIsMine = matcher.group(3);
 				boolean mine = Boolean.parseBoolean(sIsMine);
+				// Get the fourth matching group, representing whether the square has been uncovered by the user
 				String sStatus = matcher.group(4);
 				Square.STATUS status = Square.STATUS.valueOf(sStatus);
 				
+				// Build a square object from the information gathered above
 				Square square = new Square();
 				square.setMine(mine);
 				
